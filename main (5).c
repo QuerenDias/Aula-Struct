ec51aef24354abd0f40bc54e557ad7239f95bd82
QuerenDias <130005742+QuerenDias@users.noreply.github.com>
2023-04-05 20:06:20 -0300

    Add files via upload

---
+++ main (5).c
@@ -0,0 +1,23 @@
+
+#include <stdio.h>
+
+int main()
+{ struct dados_de_aluno
+{
+    char nome[50];
+    float media;
+    int ano;
+};
+  struct dados_de_aluno aluno;
+  printf("Digite o nome do aluno.");
+   fgets(aluno.nome, 50, stdin);
+  printf("Digite a media escolar.");
+   scanf("%f", &aluno.media);
+  printf("Digite o ano escolar."); 
+   scanf("%d", &aluno.ano);
+   
+  printf("Nome ...: %s", aluno.nome);
+  printf("Media ...: %f\n", aluno.media);
+  printf("Ano ...: %d\n", aluno.ano);
+    return 0;
+}
